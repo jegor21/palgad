1c4e0b0f42912acbbeb20a8df375d9e3019ac88c
Jegor <Jegor>
2024-01-18 01:54:37 +0200

    tupik

--- functionsww.py
+++ functionsww.py
@@ -78,20 +78,118 @@ def Sort(i:list,p:list,a:int):
     return i,p
 
 #5. function
+def palkasort(data, order='asc'):
+    
+    sorteerimine = sorted(data.items(), key = lambda x: x[1], reverse=(order == 'desc'))
+    return sorteerimine
+
+#lambda - 
+
+
+
 #6. function
+def saamapalk(data):
+    
+    palkami = {}
+    
+    for name, palk in data.items():
+        
+        if palk in palkami:
+            
+            palkami[palk].append(name)
+        else:
+            palkami[palk] = [name]
+
+    for palk, names in palkami.items():
+        if len(names) > 1:
+            print(f"Inimesed palkaga {palk}: {', '.join(names)}")
+    return saamapalk
+
+
+
+
+
 #7. function
+def otsipalk(data, search_name):
+   
+    palkad = [palk for name, palk in data.items() if name == search_name]
+    return palkad
+
+
+
+
+
+
 #8. function
+def filter_palk(data, threshold, mode='parem'):
+    
+    filtreerituddata = [(name, palk) for name, palk in data.items() if (mode == 'rohkem' and palk > threshold) or (mode == 'natukem' and palk < threshold)]
+
+    if filtreerituddata:
+        print(f"Inimesed palkaga {mode} kui {threshold}:")
+        for name, palk in filtreerituddata:
+            print(f"{name}: {palk}")
+    else:
+        print(f"Ei ole inimesed palkaga {mode} kui {threshold}.")
+    return filter_palk
+
+
+
 #9. function
+
+
+
+
 #10. function
+
+
+
+
 #11. function
+
+
+
+
 #12. function
+
+
+
+
 #13. function
+
+
+
+
 #14. function
+
+
+
+
 #15. function
+
+
+
+
 #16. function
+
+
+
+
 #17. function
+
+
+
+
 #18. function
+
+
+
+
 #19. function
+
+
+
+
 
 
 
