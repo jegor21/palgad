39b30778625182d81b45de6a497a39d1949f6e49
Jegor <Jegor>
2024-01-25 02:24:15 +0200

    Koik

--- functionsww.py
+++ functionsww.py
@@ -83,7 +83,6 @@ def palkasort(data, order='asc'):
     sorteerimine = sorted(data.items(), key = lambda x: x[1], reverse=(order == 'desc'))
     return sorteerimine
 
-#lambda - 
 
 
 
@@ -138,9 +137,34 @@ def filter_palk(data, threshold, mode='parem'):
 #9. function
 
 
+def Top(data):
+    
+    if not data:
+        return None, None
+
+    sorted_data = sorted(data.items(), key=lambda x: x[1])
+    mitterikkad = sorted_data[0][0]
+    rikkad = sorted_data[-1][0]
+
+    return mitterikkad, rikkad
+
+
 
 
 #10. function
+
+
+def Keskmine(data):
+    
+    if not data:
+        return None, None
+
+    keskminepalk = sum(data.values()) / len(data)
+    
+    ppalk = min(data.values(), key=lambda x: abs(x - keskminepalk))
+    inimenepalka = [name for name, palk in data.items() if palk == ppalk][0]
+
+    return keskminepalk, inimenepalka
 
 
 
@@ -148,9 +172,20 @@ def filter_palk(data, threshold, mode='parem'):
 #11. function
 
 
+def Tulumaks(data):
+   
+    maksu = {name: palk * 0.8 for name, palk in data.items()}
+    return maksu
+
 
 
 #12. function
+
+
+def sortnimi(data, order='asc'):
+    
+    sorted_data = dict(sorted(data.items(), key=lambda x: x[0], reverse=(order == 'desc')))
+    return sorted_data
 
 
 
@@ -158,19 +193,75 @@ def filter_palk(data, threshold, mode='parem'):
 #13. function
 
 
+def kustutamine(data):
+
+    if not data:
+        return []
+
+    keskminepalk = sum(data) / len(data)
+    filtered_data = [palk for palk in data if palk < keskminepalk]
+
+    return filtered_data
 
 
 #14. function
 
+
+def formeeri(data):
+    
+    formeritud = []
+
+    for i in range(0, len(data), 2):
+        nimi = data[i].capitalize()
+        palk = int(data[i + 1])
+        formeritud.extend([nimi, palk])
+
+    return formeritud
 
 
 
 #15. function
 
 
+def preemia(data, aastat, tootj=None):
+
+    if not data:
+        return {}
+
+    if tootj:
+        index = data.index(tootj)
+        palk = int(data[index + 1])
+        aastatedasi = int(palk * (1.05 ** aastat))
+        return aastatedasi
+    else:
+        bonusid = {}
+
+        for i in range(0, len(data), 2):
+            nimi = data[i]
+            palk = int(data[i + 1])
+            aastatedasi = int(palk * (1.05 ** aastat))
+            bonusid[nimi] = aastatedasi
+
+        return bonusid
+
 
 
 #16. function
+
+
+def annanimi(data):
+   
+    if not data:
+        return {}
+
+    updatet = data.copy()
+    uusnimi = list(data.keys())[2::3]
+
+    for old_name in uusnimi:
+        new_name = input(f"Pange uus nimi {old_name}: ").capitalize()
+        updatet[new_name] = updatet.pop(old_name)
+
+    return updatet
 
 
 
@@ -178,9 +269,68 @@ def filter_palk(data, threshold, mode='parem'):
 #17. function
 
 
+def RedigeeriAndmeid(andmed):
+    if not andmed:
+        return []
+
+    muudetud_andmed = []
+    
+    while True:
+        print("\nValige, mida soovite redigeerida:")
+        print("1. Nimi")
+        print("2. Palk")
+        print("3. Redigeerimine l?petatud")
+
+        valik = input("Sisestage valiku number (1, 2 v?i 3): ")
+
+        if valik == '1':
+            vana_nimi = input("Sisestage vana nimi redigeerimiseks: ")
+            uus_nimi = input("Sisestage uus nimi: ").capitalize()
+            if vana_nimi in andmed:
+                andmed[uus_nimi] = andmed.pop(vana_nimi)
+                print(f"Nimi edukalt muudetud: {vana_nimi} -> {uus_nimi}")
+            else:
+                print(f"Viga: Nime {vana_nimi} ei leitud.")
+        elif valik == '2':
+            nimi = input("Sisestage inimese nimi, kelle palga soovite muuta: ")
+            if nimi in andmed:
+                uus_palk = input(f"Sisestage uus palk {nimi} jaoks: ")
+                try:
+                    andmed[nimi] = int(uus_palk)
+                    print(f"Palk edukalt muudetud {nimi} jaoks: {uus_palk}")
+                except ValueError:
+                    print("Viga: Sisestage palga jaoks korrektne number.")
+            else:
+                print(f"Viga: Nime {nimi} ei leitud.")
+        elif valik == '3':
+            break
+        else:
+            print("Viga: Sisestage ?ige valiku number.")
+
+    muudetud_andmed = list(andmed.items())
+    return muudetud_andmed
+
+
 
 
 #18. function
+
+
+def LeiaEesnimega(data):
+    
+    if not data:
+        print("Andmed puuduvad.")
+        return
+
+    taht = input("Sisestage otsitava tahe algust?ht: ").capitalize()
+    sobivad_nimed = [nimi for nimi in data.keys() if nimi.startswith(taht)]
+
+    if not sobivad_nimed:
+        print(f"Nimesid, mis algavad t?hega '{taht}', ei leitud.")
+    else:
+        print("\nNimed ja vastavad palgad:")
+        for nimi in sobivad_nimed:
+            print(f"{nimi}: {data[nimi]}")
 
 
 
@@ -188,13 +338,11 @@ def filter_palk(data, threshold, mode='parem'):
 #19. function
 
 
+def kogupalka(data):
+    
+    if not data:
+        return 0
 
-
-
-
-
-
-
-
+    return sum(data.values())
 
 
--- palgad.py
+++ palgad.py
@@ -1,10 +1,10 @@
-﻿from functionsww import *
+from functionsww import *
 
 palgad=[1200,2500,750,395,1200]
 inimesed=["A","B","C","D","E"]
 
 while True:
-    print("Lisamine-1\nKustutamine-2\nSuurimPalk-3\nSort-5")
+    print("Lisamine-1\nKustutamine-2\nSuurimPalk-3\nKes saab suurem palka-4\nSort-5\nKes saab saama palka-6\nOtsimine-7\nNimekiri-8\nTop-9\nKeskmine-10\nTulumaks-11\nNimi sorteerimine-12\nKes saab alla keskmiselt-13\nRedigeerimine-14\nPreemia-15\nUus nimi-16\nData Redigeerimine-17\nNimi ja taht ostimine-18\nOma function-19")
     
     v=int(input())
 
@@ -21,8 +21,110 @@ while True:
     elif v==3:
         maxpalk,nimi=suurimpalk(inimesed,palgad)
         print(nimi,"saab kätte",maxpalk,"Eur")
+    
+    # elif v == 4:
+    #     i = int(input("Kasvab-0, kahaneb-1: "))
+    #     inimesed, palgad = Sort(inimesed, palgad, i)
+    #     for i in range(len(palgad)):
+    #         print(palgad[i], "on", inimesed[i], "-")
+ 
     elif v==5:
         i=int(input("Kasvab-0,kahaneb-1"))
         inimesed,palgad=Sort(inimesed,palgad,i)
         for i in range(len(palgad)):
             print(palgad[i],"on",inimesed[i],"-")
+            
+
+    elif v == 6:
+        saamapalktt = saamapalk(dict(zip(inimesed, palgad)))
+        print(saamapalktt)
+
+
+
+    elif v == 7:
+        leiaaaaa = input("Sisestage otsitava nime: ")
+        palkad_result = otsipalk(dict(zip(inimesed, palgad)), leiaaaaa)
+
+        print(palkad_result)
+
+# ...
+
+    elif v == 8:
+        threshold = int(input("Sisestage lävendi väärtus: "))
+        mode = input("Sisestage režiim ('rohkem' või 'natukem'): ")
+        filtered_result = filter_palk(dict(zip(inimesed, palgad)), threshold, mode)
+
+        print(filtered_result)
+
+
+
+    elif v == 9:
+        mitterikkad, rikkad = Top(dict(zip(inimesed, palgad)))
+        print(f"Mitterikkaim inimene: {mitterikkad}")
+        print(f"Rikkaim inimene: {rikkad}")
+
+    elif v == 10:
+        keskmine_palk, inimene = Keskmine(dict(zip(inimesed, palgad)))
+        print(f"Keskmine palk: {keskmine_palk}, Inimene: {inimene}")
+
+    elif v == 11:
+        maksu_andmed = Tulumaks(inimesed, palgad)
+        print("Tulumaksu andmed:")
+        for i in range(len(inimesed)):
+            print(f"{inimesed[i]}: {maksu_andmed[i]}")
+
+    elif v == 12:
+        sorted_names = sortnimi(inimesed)
+        print("Nimed tähestikulises järjekorras:")
+        for name in sorted_names:
+            print(name)
+            
+    elif v == 13:
+        kustutatud_palgad = kustutamine(palgad)
+        print("Palgad pärast kustutamist:")
+        for i in range(len(kustutatud_palgad)):
+            print(inimesed[i], " saab kätte", kustutatud_palgad[i], "Eur")
+            
+    elif v == 14:
+        andmed = formeeri(inimesed + palgad)
+        print("Formeeritud andmed:")
+        for i in range(0, len(andmed), 2):
+            print(andmed[i], " saab kätte", andmed[i + 1], "Eur")
+            
+    elif v == 15:
+        aastat = int(input("Mitu aastat on möödunud? "))
+        tetete = input("Kellele soovite preemiat arvutada (jätke tühi, et arvutada kõigile)? ")
+        bonusid = preemia(inimesed + palgad, aastat, tetete)
+
+        if tetete and not bonusid:
+            print(f"Viga: Nime {tetete} ei leitud.")
+        elif tetete:
+            print(f"{tetete} preemia pärast {aastat} aastat: {bonusid}Eur")
+        else:
+            print("Preemiad pärast", aastat, "aastat:")
+            for nimi, preemia in bonusid.items():
+                print(nimi, "saab preemiat", preemia, "Eur")
+                
+
+    elif v == 16:
+        updated_data = annanimi(dict(zip(inimesed, palgad)))
+        print("Uuendatud andmed:")
+        for name, salary in updated_data.items():
+            print(f"{name}: {salary}")
+
+    elif v == 17:
+        muudetud_andmed = RedigeeriAndmeid(dict(zip(inimesed, palgad)))
+        print("Uuendatud andmed:")
+        for name, salary in muudetud_andmed:
+            print(f"{name}: {salary}")
+
+
+
+    elif v == 18:
+        LeiaEesnimega(dict(zip(inimesed, palgad)))
+
+
+
+    elif v == 19:
+        summa = kogupalka(dict(zip(inimesed, palgad)))
+        print(f"Kogu palk: {summa}")
